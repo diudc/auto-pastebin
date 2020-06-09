17014ad1bc79b0294183f75e19d6b42a0f27a61c
shoccho <sfb.shoccho@gmail.com>
2020-06-09 23:40:45 +0600

    fixed a bug related to file opening C:/Users/shoccho/AppData/Local/Programs/Python/Python37/python.exe c:/Users/shoccho/Desktop/Auto_pastebin-gui/window.py

--- window.py
+++ window.py
@@ -10,7 +10,9 @@ class Ui_MainWindow(QtWidgets.QWidget):
     def setupUi(self, MainWindow):
         #window
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(410, 502)
+        MainWindow.resize(500, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
 
         #fonts
         font_calibri_14 = QtGui.QFont()
@@ -24,61 +26,94 @@ class Ui_MainWindow(QtWidgets.QWidget):
         font_code.setStyleHint(QtGui.QFont.Monospace)
         font_code.setFixedPitch(True)
         font_code.setPointSize(12)
-        
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        
-        self.paste_btn= QtWidgets.QPushButton(self.centralwidget)
-        self.paste_btn.setGeometry(QtCore.QRect(120, 360, 151, 41))
-        self.paste_btn.setObjectName("paste_btn")
-        self.paste_btn.clicked.connect(self.paster)
 
-        self.code_box = QtWidgets.QPlainTextEdit(self.centralwidget)
-        self.code_box.setGeometry(QtCore.QRect(13, 160, 381, 191))
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 500, 500))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        # self.verticalLayoutWidget.setSizePolicy(sizePolicy)
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(5, 5, 5,5)
+        self.verticalLayout.setObjectName("verticalLayout")
+
+        #h1= first horizontal layout
+        self.h1 = QtWidgets.QHBoxLayout()
+        self.h1.setSizeConstraint(QtWidgets.QLayout.SetMinimumSize)
+        self.h1.setObjectName("h1")
+        self.h1.setContentsMargins(5,5,5,5)
+        
+        self.username_lable = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.username_lable.setFont(font_calibri_14)
+        self.username_lable.setObjectName("username_lable")
+        
+        self.username_input_field = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.username_input_field.setObjectName("username_input_field")
+        
+        self.h1.addWidget(self.username_lable)
+        self.h1.addWidget(self.username_input_field)
+
+        #h2= second horizontal layout
+        self.h2 = QtWidgets.QHBoxLayout()
+        self.h2.setObjectName("h2")
+        self.h2.setContentsMargins(5,5,5,5)
+
+        self.open_file_btn = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.open_file_btn.setObjectName("open_file_btn")
+        self.open_file_btn.clicked.connect(self.openfile)
+        self.open_file_btn.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum))
+
+        self.file_name_label = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.file_name_label.setText("")
+        self.file_name_label.setObjectName("file_name_label")
+
+        self.h2.addWidget(self.open_file_btn)
+        self.h2.addWidget(self.file_name_label)
+
+        self.verticalLayout.addLayout(self.h1)
+        self.verticalLayout.addLayout(self.h2)
+        #2nd vertical layout
+        self.v3 = QtWidgets.QVBoxLayout()
+        self.v3.setObjectName("v3")
+        self.v3.setContentsMargins(5,5,5,5)
+
+        self.code_box = QtWidgets.QPlainTextEdit(self.verticalLayoutWidget)
         self.code_box.setObjectName("code_box")
         self.code_box.setFont(font_code)
         self.metrics=QtGui.QFontMetrics(font_code)
         self.code_box.setTabStopWidth(self.metrics.width(' ')*4)
         
-        self.code_box_label = QtWidgets.QLabel(self.centralwidget)
-        self.code_box_label.setGeometry(QtCore.QRect(20, 140, 47, 13))
+        self.code_box_label = QtWidgets.QLabel(self.verticalLayoutWidget)
         self.code_box_label.setFont(font_calibri_12)
         self.code_box_label.setObjectName("code_box_label")
+        
+        self.paste_btn= QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.paste_btn.setObjectName("paste_btn")
+        self.paste_btn.clicked.connect(self.paster)
+                
+        self.v3.addWidget(self.code_box_label)
+        self.v3.addWidget(self.code_box)
+        self.v3.addWidget(self.paste_btn)
 
-        self.open_file_btn = QtWidgets.QPushButton(self.centralwidget)
-        self.open_file_btn.setGeometry(QtCore.QRect(10, 90, 91, 41))
-        self.open_file_btn.setObjectName("open_file_btn")
-        self.open_file_btn.clicked.connect(self.openfile)
+        self.verticalLayout.addLayout(self.v3)
 
-        self.file_name_label = QtWidgets.QLabel(self.centralwidget)
-        self.file_name_label.setGeometry(QtCore.QRect(120, 90, 271, 41))
-        self.file_name_label.setText("")
-        self.file_name_label.setObjectName("file_name_label")
-
-        self.url_label = QtWidgets.QLabel(self.centralwidget)
-        self.url_label.setGeometry(QtCore.QRect(20, 430, 51, 21))
+        self.url_label = QtWidgets.QLabel(self.verticalLayoutWidget)
         self.url_label.setFont(font_calibri_14)
         self.url_label.setObjectName("url_label")
-
-
-        self.url_text = QtWidgets.QLabel(self.centralwidget)
-        self.url_text.setGeometry(QtCore.QRect(60, 430, 321, 21))
-        self.url_text.setFont(font_calibri_12)
+        self.url_label.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum))
+        
+        self.url_text = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.url_text.setFont(font_calibri_14)
         self.url_text.setText("")
         self.url_text.setObjectName("url_text")
-        
-        self.username_lable = QtWidgets.QLabel(self.centralwidget)
-        self.username_lable.setGeometry(QtCore.QRect(40, 30, 61, 31))
-        self.username_lable.setFont(font_calibri_14)
-        self.username_lable.setObjectName("username_lable")
 
+        self.h3 = QtWidgets.QHBoxLayout()
+        self.h3.setObjectName("h3")
 
-        self.username_input_field = QtWidgets.QLineEdit(self.centralwidget)
-        self.username_input_field.setGeometry(QtCore.QRect(120, 30, 231, 31))
-        self.username_input_field.setObjectName("username_input_field")
-        
+        self.h3.addWidget(self.url_label)
+        self.h3.addWidget(self.url_text)
+        self.h3.setContentsMargins(5,5,5,5)
+
+        self.verticalLayout.addLayout(self.h3)       
         MainWindow.setCentralWidget(self.centralwidget)
-        
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 410, 21))
         self.menubar.setObjectName("menubar")
@@ -111,10 +146,9 @@ class Ui_MainWindow(QtWidgets.QWidget):
         options |= QFileDialog.DontUseNativeDialog
         self.fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
         if self.fileName!="":
-            self.fname.setText(_translate("MainWindow", self.fileName))
+            self.file_name_label.setText(self.fileName)
             data=auto_pastebin.read_file(self.fileName)
-            self.code_box.setText(data)
-
+            self.code_box.setPlainText(data)
     def paster(self):
         data=""
         if self.username_input_field.text()=="":
